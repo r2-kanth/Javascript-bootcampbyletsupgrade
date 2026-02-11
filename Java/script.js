89c9959591b0af04eed416051f4a8c22abaeb2fb
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-02-11 20:11:55 +0530

    Printign the variables

---
+++ Java/script.js
@@ -0,0 +1,6 @@
+let a=10;
+const name ='Ravi';
+var age = 36;
+console.log('the value stored in a is :',a);
+console.log('The value stored in name is :',name);
+console.log('The value stored in age is:',age);
