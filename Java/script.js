9735968d5c880fd20650c41283372761a4f95b4d
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-02-11 20:19:52 +0530

    performed arihtmetic operations

--- Java/script.js
+++ Java/script.js
@@ -1,6 +1,15 @@
-let a=10;
+/*let a=10;
 const name ='Ravi';
 var age = 36;
 console.log('the value stored in a is :',a);
 console.log('The value stored in name is :',name);
-console.log('The value stored in age is:',age);
+console.log('The value stored in age is:',age);*/
+let a=10;
+let b=20;
+let c=30;
+var add=a+b+c;
+var sub=a+b-c;
+var mul=a*b*c;
+console.log(add);
+console.log(sub);
+console.log(mul);
