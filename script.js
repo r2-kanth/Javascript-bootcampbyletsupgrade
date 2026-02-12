d79c656ca59c3277b5804fc6521300728f8fc110
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-02-12 19:18:57 +0530

    if else for cheaking miror or adult

---
+++ script.js
@@ -0,0 +1,22 @@
+/*let a=10;
+const name ='Ravi';
+var age = 36;
+console.log('the value stored in a is :',a);
+console.log('The value stored in name is :',name);
+console.log('The value stored in age is:',age);
+let a=10;
+let b=20;
+let c=30;
+var add=a+b+c;
+var sub=a+b-c;
+var mul=a*b*c;
+console.log(add);
+console.log(sub);
+console.log(mul);*/
+let age = 28;
+if(age >= 18){
+    console.log('You are an Adult!');
+}
+else{
+    console.log('you are A Minor');
+}
